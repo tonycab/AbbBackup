bda885031f1244741c4fbb4814988c85a8b33e94
Le Cabellec Anthony <anthony.lecabellec@gmail.com>
2025-06-13 15:18:46 +0200

    Add abb ref

---
+++ PCSDK 2024/Documentation/styles/main.js
@@ -0,0 +1,5 @@
+// Make page use the whole screen by using "container-fluid" instead of "container"
+// See Bootstrap documentation for more information.
+var containers = $(".container");
+containers.removeClass("container");
+containers.addClass("container-fluid");
